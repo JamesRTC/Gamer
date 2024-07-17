d0774b891d5bbac33547e93ef76690ba474f2e45
James <rotichjames8@gmail.com>
2024-07-17 22:59:39 +0300

    setting up vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Gamer/", // Set this to your basename
+  base: "/Gamer/", // Ensure this matches your Router basename
   build: {
     outDir: "dist",
   },
