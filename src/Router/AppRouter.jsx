d3548fa3c6771af0a16aae5bf5dfc83ee2ea2f29
James <rotichjames8@gmail.com>
2024-07-17 22:43:50 +0300

    added basename back to the router

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -14,7 +14,7 @@ import { Platforms } from "../Pages/Platforms";
 
 export const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Gamer">
       {/* Set the basename to your repository name */}
       <Routes>
         <Route path="/" element={<AppLayout />}>
