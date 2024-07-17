f271a31589053efd0a39b081a72b78c3dd643cc5
James <rotichjames8@gmail.com>
2024-07-18 00:38:17 +0300

    vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", // Set the base option to './'
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure this matches your output directory
+  },
+  base: "/", // Adjust if your app is hosted in a subdirectory
 });
