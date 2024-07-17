68ae380209841a77c3e249794a1f5cc426d3c544
James <rotichjames8@gmail.com>
2024-07-18 00:23:35 +0300

    vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // Set the base option to './'
+  base: "./", // Set the base option to './'
   plugins: [react()],
 });
