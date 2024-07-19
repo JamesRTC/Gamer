ecc74f8112c7d79c5321fac5296be4a3bae278fb
James <rotichjames8@gmail.com>
2024-07-19 14:27:28 +0300

    adjusted platforms page

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -16,7 +16,7 @@ export const Button = ({
       onClick={handleClick}
       className={`px-3 py-2 ${
         type === "platform"
-          ? "text-xs max-sm:text-[8px] max-md:w-full max-md:mb-2 text-nowrap"
+          ? "max-sm:text-[8px] max-md:w-full max-md:mb-2 text-nowrap"
           : ""
       } max-sm:text-sm rounded-md cursor-pointer transition-all duration-300 hover:bg-[#ff8906] ${
         isActive ? "bg-[#ff8906]" : "bg-[#f25f4c] "
